ee82abe338285c5d41c3a65efc7050b9c03f23af
Nate Buel <natebuel29@gmail.com>
2022-03-31 21:00:39 -0500

    ufc scrper now grabs all fights from /events/completed?page=all

--- ufc_stats_scrapper/ufc_stats_scrapper/spiders/fight_scraper.py
+++ ufc_stats_scrapper/ufc_stats_scrapper/spiders/fight_scraper.py
@@ -4,18 +4,33 @@ import scrapy
 class UfcFightSpider(scrapy.Spider):
     name = "ufc_fight"
     # response.css("table.b-fight-details__table tbody tr::attr(data-link)").getall()
+    #response.css("table.b-statistics__table-events tbody tr.b-statistics__table-row i.b-statistics__table-content a::attr(href)").getall()
     start_urls = [
-        'http://ufcstats.com/event-details/1fac46d466abd5b8'
+        'http://ufcstats.com/statistics/events/completed?page=all'
     ]
 
     def parse(self, response):
+        fight_event_links = response.css(
+            "table.b-statistics__table-events tbody tr.b-statistics__table-row i.b-statistics__table-content a::attr(href)").getall()
+        yield from response.follow_all(fight_event_links, self.parse_fight_events)
+
+    def parse_fight_events(self, response):
         fight_links = response.css(
             "table.b-fight-details__table tbody tr::attr(data-link)").getall()
-        yield from response.follow_all(fight_links, self.parse_fights)
+        yield from response.follow_all(fight_links, self.parse_fight_events)
 
-    def parse_fights(self, response):
+    def parse_fight_event(self, response):
         page = response.url.split("/")[-2]
         filename = f'fights-{page}.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
+
+
+# -> returns fighter names response.css("section.b-fight-details__section table tbody td.b-fight-details__table-col p.b-fight-details__table-text a::text").getall()
+# returns fighter stats -> response.css("section.b-fight-details__section table tbody td.b-fight-details__table-col p.b-fight-details__table-text::text").getall()
+# will need to do the following things ->
+# 1. strip the strings -> stripped_list = list(map(str.strip,test_list))
+# 2. remove empty string and --- from list ->
+##while '---' in stripped_list:stripped_list.remove('---')
+# >>> while "" in stripped_list: stripped_list.remove("")
