eb6669f27bbfc60edf81763558a1c6d7c1d01767
Nate Buel <natebuel29@gmail.com>
2022-04-07 21:43:43 -0500

    implement the fighter_scraper

--- ufc_stats_scrapper/ufc_stats_scrapper/spiders/fighter_scraper.py
+++ ufc_stats_scrapper/ufc_stats_scrapper/spiders/fighter_scraper.py
@@ -1,47 +1,193 @@
 from os import stat
 import scrapy
 import numpy as np
+from datetime import date
 
 
 class UfcFightSpider(scrapy.Spider):
-    name = "ufc_fighter"
-    start_urls = [
-        'http://ufcstats.com/fighter-details/aec185309b4843d0'
-    ]
+    name = "ufc_fighters"
+    start_urls = ["http://ufcstats.com/statistics/fighters"]
 
-    # def parse(self, response):
-    #     alphabetized_fighter_links = response.css(
-    #         "section div.b-statistics__nav-inner ul li a::attr(href)").getall()
-    #     print(alphabetized_fighter_links)
-    #     yield from response.follow_all(alphabetized_fighter_links, self.fetch_all_fighters)
-
-    # def fetch_all_fighters(self, response):
-    #     all_pages = response.css(
-    #         "li.b-statistics__paginate-item a::attr(href)").getall()
-    #     yield response.follow(all_pages[len(all_pages)-1], self.parse_fighters)
-
-    # def parse_fighters(self, response):
-    #     fighters = response.css(
-    #         "section table.b-statistics__table tbody tr.b-statistics__table-row td.b-statistics__table-col a::attr(href)").getall()
-    #     yield from response.follow_all(fighters, self.parse_fighter)
+    month_map = {
+        "Jan": 1,
+        "Feb": 2,
+        "Mar": 3,
+        "Apr": 4,
+        "May": 5,
+        "Jun": 6,
+        "Jul": 7,
+        "Aug": 8,
+        "Sep": 9,
+        "Oct": 10,
+        "Nov": 11,
+        "Dec": 12,
+    }
 
     def parse(self, response):
-        name = str.strip(response.css(
-            "section span.b-content__title-highlight ::text").get())
-        nickname = str.strip(response.css(
-            "p.b-content__Nickname ::text").get())
+        alphabetized_fighter_links = response.css(
+            "section div.b-statistics__nav-inner ul li a::attr(href)"
+        ).getall()
+        print(alphabetized_fighter_links)
+        yield from response.follow_all(
+            alphabetized_fighter_links, self.fetch_all_fighters
+        )
+
+    def fetch_all_fighters(self, response):
+        all_pages = response.css(
+            "li.b-statistics__paginate-item a::attr(href)"
+        ).getall()
+        yield response.follow(all_pages[len(all_pages) - 1], self.parse_fighters)
+
+    def parse_fighters(self, response):
+        fighters = response.css(
+            "section table.b-statistics__table tbody tr.b-statistics__table-row td.b-statistics__table-col a::attr(href)"
+        ).getall()
+        yield from response.follow_all(fighters, self.parse_fighter)
+
+    def parse_fighter(self, response):
+        name = str.strip(
+            response.css("section span.b-content__title-highlight ::text").get()
+        )
+        nickname = str.strip(response.css("p.b-content__Nickname ::text").get())
         first_name = name.split(" ")[0]
         last_name = name.split(" ")[1]
 
-        record = str.strip(response.css(
-            "span.b-content__title-record ::text").get()).split(" ")[1]
+        record = str.strip(
+            response.css("span.b-content__title-record ::text").get()
+        ).split(" ")[1]
 
         fstats = response.css("li.b-list__box-list-item ::text").getall()
         fstats = self.strip_scrapped_data(fstats)
-        print(fstats)
+        fstats = np.array(fstats)
+        if fstats.size == 25:
+            fstats = np.insert(fstats, 7, "---")
+        stats_matrix = np.reshape(fstats, (13, 2))
+        height = (
+            self.convert_feet_to_inches(stats_matrix[0][1])
+            if stats_matrix[0][1] != "---" and stats_matrix[0][1] != "--"
+            else "N/A"
+        )
+        weight = (
+            int(stats_matrix[1][1].split(" lbs.")[0])
+            if stats_matrix[1][1] != "---" and stats_matrix[1][1] != "--"
+            else "N/A"
+        )
+        reach = (
+            int(stats_matrix[2][1].split('"')[0])
+            if stats_matrix[2][1] != "---" and stats_matrix[2][1] != "--"
+            else "N/A"
+        )
+        stance = (
+            stats_matrix[3][1]
+            if stats_matrix[3][1] != "---" and stats_matrix[3][1] != "--"
+            else "N/A"
+        )
+        dob = (
+            stats_matrix[4][1]
+            if stats_matrix[4][1] != "---" and stats_matrix[4][1] != "--"
+            else "N/A"
+        )
+        age = self.compute_age(dob) if dob != "N/A" else "N/A"
+        slpm = (
+            float(stats_matrix[5][1])
+            if stats_matrix[5][1] != "---" and stats_matrix[5][1] != "--"
+            else "N/A"
+        )
+        str_ac = (
+            int(stats_matrix[6][1].replace("%", "")) / 100
+            if stats_matrix[6][1] != "---" and stats_matrix[6][1] != "--"
+            else "N/A"
+        )
+        sapm = (
+            float(stats_matrix[7][1])
+            if stats_matrix[7][1] != "---" and stats_matrix[7][1] != "--"
+            else "N/A"
+        )
+        str_def = (
+            int(stats_matrix[8][1].replace("%", "")) / 100
+            if stats_matrix[8][1] != "---" and stats_matrix[8][1] != "--"
+            else "N/A"
+        )
+        td_avg = (
+            float(stats_matrix[9][1])
+            if stats_matrix[9][1] != "---" and stats_matrix[9][1] != "--"
+            else "N/A"
+        )
+        td_acc = (
+            int(stats_matrix[10][1].replace("%", "")) / 100
+            if stats_matrix[10][1] != "---" and stats_matrix[10][1] != "--"
+            else "N/A"
+        )
+        td_def = (
+            int(stats_matrix[11][1].replace("%", "")) / 100
+            if stats_matrix[11][1] != "---" and stats_matrix[11][1] != "--"
+            else "N/A"
+        )
+        sub_avg = (
+            float(stats_matrix[12][1])
+            if stats_matrix[12][1] != "---" and stats_matrix[12][1] != "--"
+            else "N/A"
+        )
+
+        # print(height)
+        # print(weight)
+        # print(reach)
+        # print(stance)
+        # print(dob)
+        # print(age)
+        # print(slpm)
+        # print(str_ac)
+        # print(sapm)
+        # print(str_def)
+        # print(td_avg)
+        # print(td_acc)
+        # print(td_def)
+        # print(sub_avg)
+
+        yield {
+            "name": name,
+            "nickname": nickname,
+            "f_name": first_name,
+            "l_name": last_name,
+            "record": record,
+            "height": height,
+            "weight": weight,
+            "reach": reach,
+            "stance": stance,
+            "dob": dob,
+            "age": age,
+            "slpm": slpm,
+            "str_ac": str_ac,
+            "sapm": sapm,
+            "str_def": str_def,
+            "td_avg": td_avg,
+            "td_acc": td_acc,
+            "td_def": td_def,
+            "sub_avg": sub_avg,
+        }
 
     def strip_scrapped_data(self, results):
         results = list(map(str.strip, results))
         while "" in results:
             results.remove("")
         return results
+
+    def null_check(self, stat):
+        if stat == "---" or stat == "--":
+            return "0"
+        else:
+            return stat
+
+    def compute_age(self, dob):
+        dob_split = dob.replace(",", "").split(" ")
+        month = self.month_map.get(dob_split[0])
+        today = date.today()
+        bday = date(int(dob_split[2]), int(month), int(dob_split[1]))
+        return (
+            today.year - bday.year - ((today.month, today.day) < (bday.month, bday.day))
+        )
+
+    def convert_feet_to_inches(self, height):
+        height = height.replace('"', "")
+        split_height = height.split("' ")
+        return int(split_height[0]) * 12 + int(split_height[1])
