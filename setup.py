6b50735f37c86f39e6f64f4e0e20f02d110dc515
Nate Buel <natebuel29@gmail.com>
2022-04-12 18:32:31 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+### READ THIS: https://docs.python.org/3/distutils/setupscript.html to setup script
+## this script
+setup(
+    name="ufc_stats_scraper",
+    version="0.0.1",
+    author="Nathan Buel",
+    packages=["ufc_stats_scraper"],
+)
