4f2a997c854759be1e8aa11dd40384ec641c6b97
Nate Buel <natebuel29@gmail.com>
2022-05-28 19:11:49 -0500

    create pipeline for fight scraper

--- ufc_stats_scraper/pipelines.py
+++ ufc_stats_scraper/pipelines.py
@@ -9,7 +9,7 @@ import mysql.connector
 from itemadapter import ItemAdapter
 
 
-class UfcStatsScraperPipeline:
+class UfcFutureFightScraperPipeline:
     connection = {
         "host": "",
         "username": "mysqlAdmin",
@@ -49,6 +49,120 @@ class UfcStatsScraperPipeline:
             item["location"],
             item["event_name"],
         )
+
+        self.cur.execute(sql, val)
+        self.con.commit()
+        return item
+
+
+class UfcFightScraperPipeline:
+    connection = {
+        "host": "",
+        "username": "mysqlAdmin",
+        "password": "",
+        "db": "thisisatest",
+    }
+
+    def __init__(self):
+        self.con = mysql.connector.connect(
+            host=self.connection["host"],
+            user=self.connection["username"],
+            password=self.connection["password"],
+            database=self.connection["db"],
+        )
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute(
+            """CREATE TABLE if not exists fights(id INT AUTO_INCREMENT PRIMARY KEY,
+            r_fighter TEXT,
+            b_fighter TEXT,
+            r_win INT,
+            b_win INT,
+            wei_class TEXT,
+            method TEXT,
+            round_ TEXT,
+            time_ TEXT,
+            t_format TEXT,
+            ref TEXT,
+            details TEXT,
+            r_kd INT,
+            b_kd INT,
+            r_sigstr FLOAT,
+            b_sigstr FLOAT,
+            r_totstr FLOAT,
+            b_totstr FLOAT,
+            r_td INT,
+            b_td INT,
+            r_sub INT,
+            b_sub INT,
+            r_rev INT,
+            b_rev INT,
+            r_ctrl INT,
+            b_ctrl INT,
+            r_hstr FLOAT,
+            b_hstr FLOAT,
+            r_bstr FLOAT,
+            b_bstr FLOAT,
+            r_lstr FLOAT,
+            b_lstr FLOAT,
+            r_dis FLOAT,
+            b_dis FLOAT,
+            r_cli FLOAT,
+            b_cli FLOAT,
+            r_gro FLOAT,
+            b_gro FLOAT)"""
+        )
+
+    def process_item(self, item, spider):
+        sql = """ INSERT IGNORE INTO fights (r_fighter,b_fighter,r_win,b_win,wei_class,method,round_,time_,t_format,ref,
+        details,r_kd,b_kd,r_sigstr,b_sigstr,r_totstr,b_totstr,r_td,b_td,r_sub,b_sub,r_rev,b_rev,r_ctrl,b_ctrl,r_hstr,b_hstr,r_bstr,
+        b_bstr,r_lstr,b_lstr,r_dis,b_dis,r_cli,b_cli,r_gro,b_gro) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        """
+
+        # TODO: Add NULL checking for fights missing parameters
+        val = (
+            item["r_fighter"],
+            item["b_fighter"],
+            item["r_win"],
+            item["b_win"],
+            item["wei_class"],
+            item["method"],
+            item["round"],
+            item["time"],
+            item["t_format"],
+            item["ref"],
+            item["details"],
+            item["r_kd"],
+            item["b_kd"],
+            item["r_sigstr"],
+            item["b_sigstr"],
+            item["r_totstr"],
+            item["b_totstr"],
+            item["r_td"],
+            item["b_td"],
+            item["r_sub"],
+            item["b_sub"],
+            item["r_rev"],
+            item["b_rev"],
+            item["r_ctrl"],
+            item["b_ctrl"],
+            item["r_hstr"],
+            item["b_hstr"],
+            item["r_bstr"],
+            item["b_bstr"],
+            item["r_lstr"],
+            item["b_lstr"],
+            item["r_dis"],
+            item["b_dis"],
+            item["r_cli"],
+            item["b_cli"],
+            item["r_gro"],
+            item["b_gro"],
+        )
+
+        print(val)
 
         self.cur.execute(sql, val)
         self.con.commit()
--- ufc_stats_scraper/settings.py
+++ ufc_stats_scraper/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    "ufc_stats_scraper.pipelines.UfcStatsScraperPipeline": 300,
+    "ufc_stats_scraper.pipelines.UfcFightScraperPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
