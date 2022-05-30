cf530cbfdfcecfb56490f09ffd722a039aa99ade
Nate Buel <natebuel29@gmail.com>
2022-05-30 12:37:06 -0500

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name="ufc_stats_scraper",
-    version="1.0.2",
+    version="1.0.3",
     author="Nathan Buel",
     packages=["ufc_stats_scraper", "ufc_stats_scraper.test_spiders"],
 )
